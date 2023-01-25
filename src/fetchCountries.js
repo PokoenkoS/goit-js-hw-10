42ebcfe36352feb5bab5af819bfccd1e6b85813e
PokoenkoS <pokoenkosveta@gmail.com>
2023-01-25 13:46:33 +0200

    add

---
+++ src/fetchCountries.js
@@ -0,0 +1,13 @@
+export default function fetchCountries (name) {
+    fetch(`https://restcountries.com/v3.1/name/${name}?fields=name.official,capital,population,flags.svg,languages`)
+    .then(response => {
+        return response.json();
+     })
+     .then(country=>{
+        console.log(country);
+     })
+     .catch(error => {
+        console.log(error);
+     })
+    
+}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 import './css/styles.css';
-
+import fetchCountries from './fetchCountries';
+import debounce from 'lodash.debounce'
 const DEBOUNCE_DELAY = 300;
+const inputEl = document.getElementById(`search-box`)
+
+inputEl.addEventListener('input',debounce(onInput, DEBOUNCE_DELAY));
+
+function onInput(e) {
+    e.preventDefault();
+const form = e.target.value;
+  
+    
+    console.log(form);
+}
+
+fetchCountries('italy');
