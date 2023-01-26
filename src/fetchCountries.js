eaad254fe917dbbacd474e5cbf1b3c67468a9aea
PokoenkoS <pokoenkosveta@gmail.com>
2023-01-26 11:27:29 +0200

    add

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,10 +1,12 @@
 export default function fetchCountries (name) {
-    fetch(`https://restcountries.com/v3.1/name/${name}?fields=name.official,capital,population,flags.svg,languages`)
+   const url = `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`;
+
+   return  fetch(url)
     .then(response => {
         return response.json();
      })
-     .then(country=>{
-        console.log(country);
+     .then(country =>{
+       console.log(country);
      })
      .catch(error => {
         console.log(error);
--- src/index.js
+++ src/index.js
@@ -1,17 +1,55 @@
 import './css/styles.css';
 import fetchCountries from './fetchCountries';
 import debounce from 'lodash.debounce'
+
 const DEBOUNCE_DELAY = 300;
-const inputEl = document.getElementById(`search-box`)
+const inputEl = document.getElementById(`search-box`);
+const countryList = document.querySelector(`.country-list`);
+const countryInfo = document.querySelector(`.country-info`)
 
 inputEl.addEventListener('input',debounce(onInput, DEBOUNCE_DELAY));
 
 function onInput(e) {
     e.preventDefault();
 const form = e.target.value;
-  
-    
-    console.log(form);
+   if (!form) {
+    clearForm();
+   }
+ 
+    fetchCountries(form)
+    .then(createCard)
+    // .catch(error => {
+    //     return alert('Oops, there is no country with that name');
+    // })
 }
 
-fetchCountries('italy');
+function createCard(country) {
+    if(country.length > 10){
+        clearForm();
+        return alert('Too many matches found. Please enter a more specific name.')
+    }  
+    if(country.length > 2 && country.length < 10) {
+        const cardList = country.map(({flags, name}) => {
+            return 
+            '<li> <img src = ${flags.svg} alt = "Country flag" width = 100><p>${name.official}</p></li>';
+    })
+    .join('');
+    countryList.innerHTML= cardList;
+    countryInfo.innerHTML = '';
+
+        }
+        if (country.length === 1) {
+            const cardInfo = country.map(({flags, name,capital,population,language}) => {
+            return 
+            '<li><img src = ${flags.svg} alt = "Country flag" width = 100><h1>${name.official}</h1><p>${capital}</p><p>${population}</p><p>${language}</p></li>';
+        }).join('');
+        countryInfo.innerHTML = cardInfo;
+        inputEl.innerHTML = '';
+    }
+
+    function clearForm() {
+        inputEl.innerHTML = '';
+        countryInfo.innerHTML = '';
+    }
+}
+        
